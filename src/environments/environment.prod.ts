48e9af3a4a38b3d7c8bac6f7227dac9c08933351
skhacker105 <skhacker105@gmail.com>
2026-06-04 16:33:54 +0530

    changed BE URL in UI environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://society-management-be.vercel.app/api/v1',
+  apiBaseUrl: 'https://societymanagementbe-n4gd.onrender.com/api/v1',
   googleMapsAPIKey: 'AIzaSyDX5bmhvA0GJFoIcKQZk7QlcY_UBPmAe-w', // 'AIzaSyASef_eQn4ThvxBhviL3mYFk9kr2btU48I'
   RAZORPAY_KEY_ID: 'rzp_test_SUeMYXlskKnEWf',
 };
